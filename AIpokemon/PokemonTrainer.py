4885e219b2aa83311fd9c13736e05db80020dbba
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2025-03-22 21:28:41 +0100

    finish

---
+++ AIpokemon/PokemonTrainer.py
@@ -0,0 +1,32 @@
+class Trainer:
+    ...
+
+
+
+
+
+
+
+
+
+
+class TrainerAI:
+    ...
+
+
+class RandomAI(TrainerAI):
+    ...
+
+
+
+class MinimaxAI(TrainerAI):
+    ...
+
+
+class MMAlphaBetaAI(MinimaxAI):
+    ...
+    
+
+
+class ExpectiMaxAI(MinimaxAI):
+    ...
